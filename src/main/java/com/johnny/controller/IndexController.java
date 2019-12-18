9bd1cc2c86779541a8a802b8a2905e5c2fbc730c
yongqiangG <874891756@qq.com>
2019-12-19 01:10:14 +0800

    logback+slf4j日志整合,输出目录D:/firmwareManager_log

--- src/main/java/com/johnny/controller/IndexController.java
+++ src/main/java/com/johnny/controller/IndexController.java
@@ -1,14 +1,43 @@
 package com.johnny.controller;
 
+import com.johnny.udp.UDPServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/firmware")
 public class IndexController {
-    @RequestMapping("/index")
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "/index")
     public String index(){
         //TODO
         return "index";
+    }
+
+    @RequestMapping(value = "/macReset")
+    @ResponseBody
+    public Map macReset(HttpServletRequest request) {
+        Map<String,Object> map = new HashMap<String,Object>();
+        String macCode = request.getParameter("macCode");
+        String port = request.getParameter("port");
+        logger.info("macCode:{}",macCode);
+        System.out.println("macCode:" + macCode);
+        try {
+            UDPServer.serverStartListen(macCode, Integer.valueOf(port));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        map.put("success",true);
+        map.put("msg","success");
+        return map;
     }
 }
---
+++ src/main/java/com/johnny/udp/Config.java
@@ -0,0 +1,16 @@
+package com.johnny.udp;
+
+public class Config {
+    /**
+     * 硬件默认端口
+     */
+    public static final int UDP_CLIENT_PORT = 3341;
+    /**
+     * UDP 响应内容大小
+     */
+    public static final int UDP_RECEIVE_LEN = 5 * 1024 ;
+    /**
+     * 服务端监听端口
+     */
+    public static final int UDP_SERVER_LISTEN_PORT = 3339;
+}
